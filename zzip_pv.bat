6ccc87a408058f91a2e03d00b15ad6cab07c6e1b
deep-soft <deep-soft@users.noreply.github.com>
2023-08-02 15:55:21 +0300

    Create zzip_pv.bat

---
+++ zzip_pv.bat
@@ -0,0 +1,1 @@
+7z a -tzip AltSnap_src.zip AltSnap.txt AltSnap.ini AltSnap.xml altsnap.nsi License.txt altsnap.c altsnap.rc config.c hooks.c hooks.rc languages.c languages.h resource.h tray.c hooks.h unfuck.h nanolibc.h window.rc AltSnap.exe.manifest makefile media\* Lang\*
