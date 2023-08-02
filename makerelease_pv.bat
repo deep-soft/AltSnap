0ea720df7037168207c2381933a28924ac7b8178
deep-soft <deep-soft@users.noreply.github.com>
2023-08-02 16:18:11 +0300

    Update makerelease_pv.bat

--- makerelease_pv.bat
+++ makerelease_pv.bat
@@ -8,25 +8,26 @@ del AltSnap%version%src.zip
 del AltSnap%version%bin.zip
 del AltSnap%version%-x64-inst.exe
 
+:: make i686 version
+make clean
+make
+call nsi.bat
+call ziprelease_pv.bat
+rename AltSnap_bin.zip AltSnap%version%bin.zip
+
+:: make x64 version
 make -fMakefileX64 clean
 make -fMakefileX64
 call nsi.bat
 rename AltSnap%VERSION%-inst.exe AltSnap%VERSION%-x64-inst.exe 
-
 call ziprelease_pv.bat
 rename AltSnap_bin.zip AltSnap%version%bin_x64.zip
 
-:: make clean
-:: make
-:: call nsi.bat
-
+:: need nsi installed
 :: rhash --sha256 AltSnap%VERSION%-x64-inst.exe > SHA256.TXT
 :: rhash --sha256 AltSnap%VERSION%-inst.exe >> SHA256.TXT
-sha256sum AltSnap%VERSION%-x64-inst.exe > SHA256.TXT
-sha256sum AltSnap%VERSION%-inst.exe >> SHA256.TXT
-
-call ziprelease_pv.bat
-rename AltSnap_bin.zip AltSnap%version%bin.zip
+:: sha256sum AltSnap%VERSION%-x64-inst.exe > SHA256.TXT
+:: sha256sum AltSnap%VERSION%-inst.exe >> SHA256.TXT
 
 call zzip_pv.bat
 rename AltSnap_src.zip AltSnap%version%src.zip
